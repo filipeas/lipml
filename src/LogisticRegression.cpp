e84a2ac0f9554ec36433a25c8ea92282a8410520
filipeas <felipealvessampaio@hotmail.com>
2024-05-02 23:32:30 -0300

    create some function into logistic regression class

--- src/LogisticRegression.cpp
+++ src/LogisticRegression.cpp
@@ -3,8 +3,24 @@
 
 using namespace std;
 
-class LinearRegression {
+class LogisticRegression {
     private:
+        vector<double> weights;
+        double learningRate;
+
     public:
-    
+        LogisticRegression(int numFeatures, double learningRate): learningRate(learningRate) {
+            // iniciando weights randomicamente
+            for (int i = 0; i < numFeatures; i++) {
+                weights.push_back((double) rand() / RAND_MAX);
+            }
+        }
+
+        double sigmoid(double x) {
+            return 1.0 / (1.0 + exp(-x));
+        }
+
+        void train(vector<vector<double>> &X, vector<int> &y, int numIterations) {
+            
+        }
 };
