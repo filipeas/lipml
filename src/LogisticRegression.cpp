a39cf88ef586e484d354caf5232dd9cb77286f97
filipeas <felipealvessampaio@hotmail.com>
2024-05-04 19:35:53 -0300

    finish default logistic regression

--- src/LogisticRegression.cpp
+++ src/LogisticRegression.cpp
@@ -5,22 +5,102 @@ using namespace std;
 
 class LogisticRegression {
     private:
-        vector<double> weights;
+        vector<vector<double>> X;
+        vector<double> y;
+        vector<double> betas;
         double learningRate;
+        int iterations;
+        int typeErrorFunction; // 0 - normal | 1 - LDA | 2 - QDA | 3 - Naive Bayes
 
-    public:
-        LogisticRegression(int numFeatures, double learningRate): learningRate(learningRate) {
-            // iniciando weights randomicamente
-            for (int i = 0; i < numFeatures; i++) {
-                weights.push_back((double) rand() / RAND_MAX);
+        double sigmoid(double z) {
+            return 1 / (1 + exp(-z));
+        }
+
+        void DefaultLogisticRegression(const vector<double> &features, vector<double> &weights, double error) {
+            // default logistic regression (entropia cruzada)
+            for (int j = 0; j < features.size(); j++) {
+                weights[j] += features[j] * error; // gradient of weights
             }
         }
 
-        double sigmoid(double x) {
-            return 1.0 / (1.0 + exp(-x));
+        void GradientDescent() {
+            int n = X.size(); // number of samples
+            int numParameters = betas.size(); // number of parameters
+            vector<double> weights(numParameters, 0.0); // start all parameters with 0
+
+            for (int i = 0; i < n; i++){
+                double y_pred = Predict(X[i]); // compute predict value by model
+
+                double error = y[i] - y_pred;
+
+                if (typeErrorFunction == 0){
+                    // compute default logistic regression
+                    DefaultLogisticRegression(X[i], weights, error);
+                } else if (typeErrorFunction == 1) {
+                    // compute LDA
+                    // LDA(X[i], weights, error);
+                } else if (typeErrorFunction == 2) {
+                    // compute QDA
+                    // QDA(X[i], weights, error);
+                } else if (typeErrorFunction == 3) {
+                    // compute Naive Bayes
+                    // NaiveBayes(X[i], weights, error);
+                }
+            }
+
+            // update weights
+            for (int i = 0; i < numParameters; i++) {
+                betas[i] += (weights[i] / n) * learningRate;
+            }
         }
 
-        void train(vector<vector<double>> &X, vector<int> &y, int numIterations) {
-            
+        double Compute_CrossEntropy () {
+            double totalError = 0.0;
+            int n = X.size();
+
+            for (int i = 0; i < n; i++){
+                double y_pred = Predict(X[i]); // compute predict value by model
+
+                totalError += -(y[i] * log(y_pred) + (1 - y[i]) * log(1 - y_pred)); // cross entropy
+            }
+
+            return totalError / n;
+        }
+    public:
+        LogisticRegression(vector<vector<double>> &X, vector<double> &y, double learningRate, int iterations, int typeErrorFunction)
+        : X(X), y(y), learningRate(learningRate), iterations(iterations), typeErrorFunction(typeErrorFunction) {
+            betas.resize(X[0].size(), 0.0);
+        }
+
+        void Train() {
+            for (int i = 0; i < iterations; i++) {
+                GradientDescent();
+                if (typeErrorFunction == 0) {
+                    // default logistic regression
+                    cout << "Iteration " << i + 1 << ", CrossEntropy_train: " << Compute_CrossEntropy() << endl;
+                } else if (typeErrorFunction == 1) {
+                    // LDA
+                    // cout << "Iteration " << i + 1 << ", MAE_train: " << Compute_MAE() << endl;
+                } else if (typeErrorFunction == 2) {
+                    // QDA
+                    // cout << "Iteration " << i + 1 << ", MAE_train: " << Compute_MAE() << endl;
+                } else if (typeErrorFunction == 3) {
+                    // Naive Bayes
+                    // cout << "Iteration " << i + 1 << ", MAE_train: " << Compute_MAE() << endl;
+                }
+            }
+        }
+
+        double Predict(const vector<double> &features) {
+            double z = betas[0]; // logits (b0 + b1x1 + b2x2 + ...)
+            for (size_t j = 0; j < features.size(); j++){
+                z += betas[j + 1] * features[j]; // coeficients of parameters
+            }
+
+            return sigmoid(z);
+        }
+
+        vector<double> getFinalParameters() const {
+            return betas;
         }
 };
--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@
 #include "./data-structures/LinkedList.cpp"
 
 #include "LinearRegression.cpp"
+#include "LogisticRegression.cpp"
 
 using namespace std;
 
@@ -14,9 +15,37 @@ int main()
 {
     printf("Hello World");
 
-    // Dados de exemplo
-    // vector<vector<double>> X = {{1, 2}, {2, 3}, {3, 4}, {4, 5}, {5, 6}}; // Features
-    // vector<double> y = {3, 4, 5, 6, 7}; // Rótulos
+    // // Dados de exemplo para Linear Regression
+    // // vector<vector<double>> X = {{1, 2}, {2, 3}, {3, 4}, {4, 5}, {5, 6}}; // Features
+    // // vector<double> y = {3, 4, 5, 6, 7}; // Rótulos
+    // vector<vector<double>> X = {{0, 0}, {0, 1}, {1, 0}, {1, 1}}; // Features
+    // vector<double> y = {0, 0, 0, 1}; // Rótulos
+
+    // // Parâmetros de treinamento
+    // double taxaAprendizado = 0.01; // Taxa de aprendizado
+    // int numIteracoes = 1000; // Número de iterações de treinamento
+
+    // // Criar objeto de regressão linear e treinar o modelo
+    // LinearRegression modelo(X, y, taxaAprendizado, numIteracoes, 0);
+    // modelo.Train();
+    
+    // // Obter e exibir os parâmetros finais do modelo
+    // vector<double> parametrosFinais = modelo.getFinalParameters();
+    // cout << "Parâmetros finais: ";
+    // for (size_t i = 0; i < parametrosFinais.size(); ++i) {
+    //     cout << "beta_" << i << " = " << parametrosFinais[i] << " ";
+    // }
+    // cout << endl;
+
+    // // exemplo de previsao
+    // vector<double> sample = {{0, 0}};
+    // double prediction = modelo.Predict(sample);
+    // cout << "Probabilidade da amostra pertencer à classe positiva: " << prediction << endl;
+
+
+    // ---------------------------
+
+    // Dados de exemplo para Logistic Regression simples
     vector<vector<double>> X = {{0, 0}, {0, 1}, {1, 0}, {1, 1}}; // Features
     vector<double> y = {0, 0, 0, 1}; // Rótulos
 
@@ -25,9 +54,9 @@ int main()
     int numIteracoes = 1000; // Número de iterações de treinamento
 
     // Criar objeto de regressão linear e treinar o modelo
-    LinearRegression modelo(X, y, taxaAprendizado, numIteracoes, 0);
+    LogisticRegression modelo(X, y, taxaAprendizado, numIteracoes, 0);
     modelo.Train();
-    
+
     // Obter e exibir os parâmetros finais do modelo
     vector<double> parametrosFinais = modelo.getFinalParameters();
     cout << "Parâmetros finais: ";
@@ -36,5 +65,12 @@ int main()
     }
     cout << endl;
 
+    // exemplo de previsao
+    vector<vector<double>> test_data = {{0, 0}, {0, 1}, {1, 0}, {1, 1}};
+    cout << "Tabela Verdade AND:" << endl;
+    for (size_t i = 0; i < test_data.size(); ++i) {
+        double prediction = modelo.Predict(test_data[i]);
+        cout << "Para entrada (" << test_data[i][0] << ", " << test_data[i][1] << "), a previsão é: " << prediction << endl;
+    }
     return 0;
 }
